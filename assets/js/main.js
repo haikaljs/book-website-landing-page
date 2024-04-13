1b1fdaae03425b997250abe7b9f635bac5c5fdfc
haikaljs <haikaljavascript@gmail.com>
2024-04-13 15:48:02 +0800

    Featured swipper

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,6 +79,27 @@ let swiperHome = new Swiper(".home__swiper", {
 
 /*=============== FEATURED SWIPER ===============*/
 
+let swiperFeatured = new Swiper(".featured__swiper", {
+  loop: true,
+  spaceBetween: 16,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    1150: {
+      slidesPerView: 4,
+      centeredSlides: false,
+    },
+  },
+});
+
 /*=============== NEW SWIPER ===============*/
 
 /*=============== TESTIMONIAL SWIPER ===============*/
