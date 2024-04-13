650e9fc67f2b0ec8a6600c3029a5566f9d275a34
haikaljs <haikaljavascript@gmail.com>
2024-04-13 13:23:53 +0800

    Login

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,56 @@
 /*=============== SEARCH ===============*/
+const searchButton = document.getElementById("search-button"),
+  searchClose = document.getElementById("search__close"),
+  searchContent = document.getElementById("search-content");
 
+/* Search show */
+if (searchButton) {
+  searchButton.addEventListener("click", () => {
+    searchContent.classList.add("show-search");
+  });
+}
+
+/* Search hidden */
+if (searchClose) {
+  searchClose.addEventListener("click", () => {
+    searchContent.classList.remove("show-search");
+  });
+}
 
 /*=============== LOGIN ===============*/
 
+const loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close"),
+  loginContent = document.getElementById("login-content");
+
+/* Login show */
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    loginContent.classList.add("show-login");
+  });
+}
+
+/* Login hidden */
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    loginContent.classList.remove("show-login");
+  });
+}
 
 /*=============== ADD SHADOW HEADER ===============*/
 
-
 /*=============== HOME SWIPER ===============*/
-
 
 /*=============== FEATURED SWIPER ===============*/
 
-
 /*=============== NEW SWIPER ===============*/
-
 
 /*=============== TESTIMONIAL SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
